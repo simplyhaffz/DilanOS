3c35bd6035099b1569b1418d1aaf5fcba718ef2b
ItsMeHaffz <privatezaidtaosif@gmail.com>
2024-10-25 20:51:06 +0600

    Delete scripts directory

--- scripts/showHide.js
+++
@@ -1,8 +0,0 @@
-function showHideDiv(name) {
-  var x = document.getElementById("show_hide" + "_" + name);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
